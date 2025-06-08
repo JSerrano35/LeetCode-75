6af490b8d8768faeb80a6c46d2786fc5e6f016ac
Juan Felipe Serrano <167997864+JSerrano35@users.noreply.github.com>
2025-06-08 14:05:26 -0500

    Initial Commit

---
+++ 1768. Merge Strings Alternately.java
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        
+    }
+}
