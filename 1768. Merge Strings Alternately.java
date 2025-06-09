93be9e16cde86def421b57404c2ddd6985afcc6f
Juan Felipe Serrano <167997864+JSerrano35@users.noreply.github.com>
2025-06-09 11:18:59 -0500

    Update 1768. Merge Strings Alternately.java

--- 1768. Merge Strings Alternately.java
+++ 1768. Merge Strings Alternately.java
@@ -2,6 +2,31 @@
 
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        
+        String sol= "";
+        if (word1.length() > word2.length()){
+            for(int i =0 ; i<word1.length();i++){
+                char char1 = word1.charAt(i);
+                sol+= char1;
+                if(i < word2.length()){
+                    char char2 = word2.charAt(i);
+                    sol += char2;
+                }
+            }
+        }
+        else{
+            for(int i =0 ; i<word2.length();i++){
+                char char2 = word2.charAt(i);
+                if(i < word1.length()){
+                    char char1 = word1.charAt(i); 
+                    sol += char1;
+                }
+                sol+= char2;
+            }
+        }
+    return sol;
     }
 }
+
+/* NOTES:
+could be faster if we just append the rest of the word instead of just keep iterating while ignoring the other word
+*/ 
