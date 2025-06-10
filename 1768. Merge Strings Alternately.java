1ccc151fc6122ca4ea2bafe18c84e347063d0ed9
Juan Felipe Serrano <167997864+JSerrano35@users.noreply.github.com>
2025-06-10 09:17:02 -0500

    Reapply "June 6 practice"
    This reverts commit 7f535b5316336acac10982e521ec3f15ed497876.

--- 1768. Merge Strings Alternately.java
+++ 1768. Merge Strings Alternately.java
@@ -3,11 +3,14 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         String sol= "";
+        // get the longest string and iterate over the bigger one
         if (word1.length() > word2.length()){
             for(int i =0 ; i<word1.length();i++){
                 char char1 = word1.charAt(i);
+                // add first character
                 sol+= char1;
                 if(i < word2.length()){
+                    // if it exists add the second character
                     char char2 = word2.charAt(i);
                     sol += char2;
                 }
@@ -18,8 +21,10 @@ class Solution {
                 char char2 = word2.charAt(i);
                 if(i < word1.length()){
                     char char1 = word1.charAt(i); 
+                    // Add the first if it exists
                     sol += char1;
                 }
+                // Add teh second
                 sol+= char2;
             }
         }
